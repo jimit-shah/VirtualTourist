bcbf66fe9fd79523ca0bea61724999d3d8251b96
Jimit Shah <jshah11@icloud.com>
2017-12-01 21:37:47 -0800

    first commit

---
+++ VirtualTourist/Controllers/PhotoAlbumViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoAlbumViewController.swift
+//  VirtualTourist
+//
+//  Created by Jimit Shah on 12/1/17.
+//  Copyright © 2017 Jimit Shah. All rights reserved.
+//
+
+import UIKit
+
+class PhotoAlbumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+  
+}
---
+++ VirtualTourist/Controllers/TravelLocationsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Jimit Shah on 12/1/17.
+//  Copyright © 2017 Jimit Shah. All rights reserved.
+//
+
+import UIKit
+
+class TravelLocationsViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  
+}
+
