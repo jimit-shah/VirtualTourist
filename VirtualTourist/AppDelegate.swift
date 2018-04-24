56d9c52f5d3d5e4f0bfe35c1e6e1f290f99588c4
Jimit Shah <jshah11@icloud.com>
2018-04-23 23:36:56 -0700

    launch app with user default settings and save region for maps.

--- VirtualTourist/AppDelegate.swift
+++ VirtualTourist/AppDelegate.swift
@@ -7,15 +7,20 @@
 //
 
 import UIKit
+import MapKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
+  struct UserDefaultKeys {
+    static let HasLaunchedBefore = "hasLaunchedBefore"
+    static let MapViewRegion = "mapViewRegion"
+  }
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
+    checkIfFirstLaunch()
     return true
   }
 
@@ -41,6 +46,34 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
   }
 
+
+  // MARK: Helper Method
+  
+  func checkIfFirstLaunch() {
+    if UserDefaults.standard.bool(forKey: UserDefaultKeys.HasLaunchedBefore) {
+      print("App has launched before")
+    } else {
+      print("This is the first launch ever!")
+      UserDefaults.standard.set(true, forKey: UserDefaultKeys.HasLaunchedBefore)
+      
+      let lat = 40.1764619
+      let lon = -100.2668550
+      let radius = 5000000.0
+      
+      let coordinate = CLLocationCoordinate2DMake(lat, lon)
+      let region = MKCoordinateRegionMakeWithDistance(coordinate, radius, radius)
+      
+      // Set default to show entire US
+      UserDefaults.standard.set([
+        "latitude": region.center.latitude,
+        "longitude": region.center.longitude,
+        "latitudeDelta": region.span.latitudeDelta,
+        "longitudeDelta": region.span.longitudeDelta
+        ], forKey: UserDefaultKeys.MapViewRegion)
+      UserDefaults.standard.synchronize()
+    }
+  }
+  
 
 }
 
--- VirtualTourist/Controllers/TravelLocationsViewController.swift
+++ VirtualTourist/Controllers/TravelLocationsViewController.swift
@@ -7,13 +7,69 @@
 //
 
 import UIKit
+import MapKit
 
 class TravelLocationsViewController: UIViewController {
 
+  // MARK: Properties
+  var annotations = [MKPointAnnotation]()
+  
+  
+  // MARK: Outlets
+  
+  @IBOutlet weak var mapView: MKMapView!
+  
+  
+  // MARK: Lifecycle
+  
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    // load region from user defaults.
+    let savedRegion = UserDefaults.standard.object(forKey: AppDelegate.UserDefaultKeys.MapViewRegion) as! [String: Any]
+    
+    let region = MKCoordinateRegion(
+      center: CLLocationCoordinate2DMake(
+        savedRegion["latitude"] as! Double,
+        savedRegion["longitude"] as! Double
+      ),
+      span: MKCoordinateSpan(
+        latitudeDelta: savedRegion["latitudeDelta"] as! Double,
+        longitudeDelta: savedRegion["longitudeDelta"] as! Double
+      )
+    )
+    
+    mapView.setRegion(region, animated: false)
+    mapView.setCenter(region.center, animated: true)
+    mapView.delegate = self
+    
   }
 
   
+  // MARK: Helper Methods
+  
+  let regionRadius: CLLocationDistance = 1000
+  func centerMapOnLocation(location: CLLocation) {
+    let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius, regionRadius)
+    mapView.setRegion(coordinateRegion, animated: true)
+  }
+  
+  func saveMapViewRegion(_ region: MKCoordinateRegion) {
+    UserDefaults.standard.set([
+      "latitude": region.center.latitude,
+      "longitude": region.center.longitude,
+      "latitudeDelta": region.span.latitudeDelta,
+      "longitudeDelta": region.span.longitudeDelta
+      ], forKey: AppDelegate.UserDefaultKeys.MapViewRegion)
+  }
+}
+
+// MARK: - TravelLocationsViewController: MKMapViewDelegate
+
+extension TravelLocationsViewController: MKMapViewDelegate {
+  func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+    saveMapViewRegion(mapView.region)
+    
+  }
 }
 
