b8788609f0bb1262e1b82f5eb27b5c04f1669a58
Jimit Shah <jshah11@icloud.com>
2020-02-26 21:16:36 -0800

    Update FlickrConstants.swift

--- VirtualTourist/Models/Clients/FlickrConstants.swift
+++ VirtualTourist/Models/Clients/FlickrConstants.swift
@@ -14,7 +14,7 @@ extension FlickrClient {
   struct Constants {
     
     // MARK: API Keys
-    static let ApiKey = "6be4a538b0f7b3dc19f5a2182528c4cd"
+    static let ApiKey = "API KEY GOES HERE"
     
     // MARK: URLs
     static let ApiScheme = "https"
